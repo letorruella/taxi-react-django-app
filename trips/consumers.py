05e27821690f4b137bc30fa7838f5b9e174f40f1
Luis E Torruella <28609995+letorruella@users.noreply.github.com>
2022-12-26 18:15:02 -0500

    web-socket-3

--- trips/consumers.py
+++ trips/consumers.py
@@ -33,6 +33,14 @@ class TaxiConsumer(AsyncJsonWebsocketConsumer):
                 status=Trip.COMPLETED
             ).only('id').values_list('id', flat=True)
         return map(str, trip_ids)
+    
+    # new
+    @database_sync_to_async
+    def _update_trip(self, data):
+        instance = Trip.objects.get(id=data.get('id'))
+        serializer = TripSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        return serializer.update(instance, serializer.validated_data)
 
     async def connect(self):
         user = self.scope['user']
@@ -96,6 +104,34 @@ class TaxiConsumer(AsyncJsonWebsocketConsumer):
             await self.create_trip(content)
         elif message_type == 'echo.message':
             await self.echo_message(content)
+        elif message_type == 'update.trip': # new
+            await self.update_trip(content)
+
+    async def update_trip(self, message):
+        data = message.get('data')
+        trip = await self._update_trip(data)
+        trip_id = f'{trip.id}'
+        trip_data = await self._get_trip_data(trip)
+
+        # Send update to rider.
+        await self.channel_layer.group_send(
+            group=trip_id,
+            message={
+                'type': 'echo.message',
+                'data': trip_data,
+            }
+        )
+
+        # Add driver to the trip group.
+        await self.channel_layer.group_add(
+            group=trip_id,
+            channel=self.channel_name
+        )
+
+        await self.send_json({
+            'type': 'echo.message',
+            'data': trip_data
+        })
 
     async def create_trip(self, message):
         data = message.get('data')
