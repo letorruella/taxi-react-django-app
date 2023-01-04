3d684b987de7a4687d5334fca02aca7fbe22c58c
Luis E Torruella <28609995+letorruella@users.noreply.github.com>
2023-01-03 19:47:52 -0500

    http request signup

--- client/cypress/e2e/authentication.cy.js
+++ client/cypress/e2e/authentication.cy.js
@@ -38,13 +38,26 @@ describe("Authentication", function () {
   });
 
   it("Can sign up.", function () {
+
+    cy.intercept('POST', 'sign_up', {
+      statusCode: 201,
+      body: {
+        id: 1,
+        username: 'gary.cole@example.com',
+        first_name: 'Gary',
+        last_name: 'Cole',
+        group: 'driver',
+        photo: '/media/images/photo.jpg'
+      }
+    }).as('signUp');
+
     cy.visit("/#/sign-up");
-    cy.get("input#username").type("gary.cole@example.com");
+    //cy.get("input#username").type("gary.cole@example.com");
     cy.get("input#firstName").type("Gary");
     cy.get("input#lastName").type("Cole");
     cy.get("input#password").type("pAssw0rd", { log: false });
     cy.get("select#group").select("driver");
-    cy.get("input#photo").attachFile("images/photo.jpg");
+    //cy.get("input#photo").attachFile("images/photo.jpg");
     cy.get("button").contains("Sign up").click();
     cy.hash().should("eq", "#/log-in");
   });
@@ -54,4 +67,31 @@ describe("Authentication", function () {
     cy.visit("/#/sign-up");
     cy.hash().should("eq", "#/");
   });
+
+  it('Show invalid fields on sign up error.', function () {
+    cy.intercept('POST', 'sign_up', {
+      statusCode: 400,
+      body: {
+        username: [
+          'A user with that username already exists.'
+        ]
+      }
+    }).as('signUp');
+    cy.visit('/#/sign-up');
+    // cy.get('input#username').type('gary.cole@example.com');
+    cy.get('input#firstName').type('Gary');
+    cy.get('input#lastName').type('Cole');
+    cy.get('input#password').type('pAssw0rd', { log: false });
+    cy.get('select#group').select('driver');
+  
+    // Handle file upload
+    //cy.get('input#photo').attachFile('images/photo.jpg');
+    cy.get('button').contains('Sign up').click();
+    cy.wait('@signUp');
+    cy.get('div.invalid-feedback').contains(
+      'A user with that username already exists'
+    );
+    cy.hash().should('eq', '#/sign-up');
+  });
+
 });
